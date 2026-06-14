cc9486d04da20b84ec84468d1974c59d75bb5cf9
Zakhar Skorokhodov <zaharsk@gmail.com>
2026-06-14 17:42:08 +0000

    fix: Bump version
    Signed-off-by: Zakhar Skorokhodov <zaharsk@gmail.com>

--- major.sh
+++ major.sh
@@ -2,8 +2,8 @@
 CURRENT_VERSION=$1
 CURRENT_MAJOR=$(echo $CURRENT_VERSION | awk -F. '{print $1}')
 if ! git rev-parse $CURRENT_MAJOR >/dev/null 2>&1; then
-    git tag $CURRENT_MAJOR
-    git push origin $CURRENT_MAJOR
+  git tag $CURRENT_MAJOR
+  git push origin $CURRENT_MAJOR
 else
   echo "$CURRENT_MAJOR already exists, skipping tagging"
 fi
